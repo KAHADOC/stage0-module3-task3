96c8792a6f385d69cd477c2a89f15b8576544ac8
Eugene KAHADOC <eugene.bespalov@gmail.com>
2023-03-14 18:19:31 +0300

    all three completed

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,19 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.print(first + "\n" + second + "\n" + third + "\n");
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.print(first + "\n" + second + "\n" + third + "\n");
+        System.out.print(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird + "\n");
+
     }
 }
